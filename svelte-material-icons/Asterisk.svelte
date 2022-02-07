ab1119b1dc2913312966b1a1fc4293f4f630afd2
Tim Ötting <email@tim-oetting.de>
2022-02-08 00:41:08 +0100

    Updated Asterisk
    Updated Asterisk icon to match icon from https://materialdesignicons.com/icon/asterisk

--- svelte-material-icons/Asterisk.svelte
+++ svelte-material-icons/Asterisk.svelte
@@ -6,4 +6,4 @@
   export let viewBox = "0 0 24 24";
 </script>
 
-<svg width="{width}" height="{height}" viewBox="{viewBox}"><path d="M10,2H14L13.21,9.91L19.66,5.27L21.66,8.73L14.42,12L21.66,15.27L19.66,18.73L13.21,14.09L14,22H10L10.79,14.09L4.34,18.73L2.34,15.27L9.58,12L2.34,8.73L4.34,5.27L10.79,9.91L10,2Z" fill="{color}"/></svg>
+<svg {width} {height} {viewBox}><path d="M21 13H14.4L19.1 17.7L17.7 19.1L13 14.4V21H11V14.3L6.3 19L4.9 17.6L9.4 13H3V11H9.6L4.9 6.3L6.3 4.9L11 9.6V3H13V9.4L17.6 4.8L19 6.3L14.3 11H21V13Z" fill={color}/></svg>
