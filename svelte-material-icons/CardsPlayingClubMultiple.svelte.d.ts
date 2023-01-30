0feafb9c30e75002651daba2529ca750a2b44a85
Ramiro <ramiroaisen@gmail.com>
2023-01-30 19:31:52 -0300

    fix class

--- svelte-material-icons/CardsPlayingClubMultiple.svelte.d.ts
+++ svelte-material-icons/CardsPlayingClubMultiple.svelte.d.ts
@@ -4,7 +4,6 @@ export default class Icon extends SvelteComponentTyped<{
   size?: string | number
   width?: string | number
   height?: string | number
-  class?: string
   color?: string
   viewBox?: string
   class?: string
